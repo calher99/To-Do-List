46c7f27a9bc95317c27ba4684217e0a20bfcf50c
calher99 <calher99@gmail.com>
2022-11-09 17:57:21 +0000

    Set up environment

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index:'./src/index.js',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'The Garden Restaurant',
+    }),
+  ],
+
+  devtool: 'inline-source-map', //To trace errors to entry file
+
+  
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      // To add the css
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      // To add images
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      // To add fonts
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+}; 
