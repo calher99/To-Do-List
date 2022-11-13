96e83a0101680d3b3ef63caf2aab2300048004e0
calher99 <calher99@gmail.com>
2022-11-13 16:02:38 +0000

    create draft in index for modal functioning

--- src/index.js
+++ src/index.js
@@ -9,12 +9,41 @@ const toDo1 = toDoFactory();
 toDo1.setTitle("Compra");
 console.log(toDo1.getTitle());
 
-const addToDo = document.querySelector("#addToDo")
-addToDo.addEventListener('click' , () => {
-    const modalToDo = document.querySelector("#modal");
-    modalToDo.classList.add('active');
-    const overlay = document.querySelector("#overlay");
-    overlay.classList.add('active');
+const openModalButtons = document.querySelectorAll("[data-open-modal]");
+const closeModalButtons = document.querySelectorAll("[data-close-modal]")
+const overlay = document.querySelector('#overlay');
+
+openModalButtons.forEach( button =>{
+    button.addEventListener('click' , (e) => {
+        const idOpen = e.target.dataset.openModal;
+        const modalToOpen = document.querySelector(idOpen);
+        openModal(modalToOpen);
+    })
 })
 
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () =>{
+        const modalToClose= button.closest('.modal');
+        closeModal(modalToClose);
+    });
 
+});
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+      closeModal(modal)
+    })
+  })
+  
+
+function openModal (modal) {
+    modal.classList.add('active');
+    // Overlay is already declared global function
+    overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
